df47f6452e43a3f4f8ef56d0744f6c2893b3726f
saynarah <saynarah.nabuco@gmail.com>
2025-10-18 14:02:57 -0300

    feat: create entities

---
+++ src/main/java/com/saynarah/api_twitter_simplificado/entities/Tweet.java
@@ -0,0 +1,24 @@
+package com.saynarah.api_twitter_simplificado.entities;
+
+import jakarta.persistence.*;
+
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tweets")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+
+    private Instant creationTimestamp;
+}
---
+++ src/main/java/com/saynarah/api_twitter_simplificado/entities/User.java
@@ -0,0 +1,60 @@
+package com.saynarah.api_twitter_simplificado.entities;
+
+import jakarta.persistence.*;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID uuid;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+}
