c2754b34025fffb8fb867abc2441b9b91e567adc
saynarah <saynarah.nabuco@gmail.com>
2025-10-18 13:59:59 -0300

    Initial commit

---
+++ src/main/java/com/saynarah/api_twitter_simplificado/ApiTwitterSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.saynarah.api_twitter_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTwitterSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTwitterSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saynarah/api_twitter_simplificado/ApiTwitterSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saynarah.api_twitter_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTwitterSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
